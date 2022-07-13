6ac6097dead376b12e78cca94c6bd1dab8bc810a
고 성진 <w8348@naver.com>
2022-07-14 08:58:48 +0900

    숫자 카드(진행중)

---
+++ 집합과 맵/10815.py
@@ -0,0 +1,12 @@
+'''
+숫자 카드
+카드의 집합을 만들어 특정 카드가 집합에 있는지 빠르게 찾는 문제
+'''
+
+import sys
+
+N = int(sys.stdin.readline())
+arr = list(map(int, sys.stdin.readline().split()))
+M = int(sys.stdin.readline())
+arr2 = list(map(int, sys.stdin.readline().split()))
+
