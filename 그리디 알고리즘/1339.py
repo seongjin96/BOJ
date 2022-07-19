b347531058f6981770a47ebcdb01ab706d4d113b
고 성진 <w8348@naver.com>
2022-07-20 08:59:47 +0900

    단어 수학(진행중)

--- 그리디 알고리즘/1339.py
+++ 그리디 알고리즘/1339.py
@@ -7,9 +7,14 @@ import sys
 N = int(sys.stdin.readline())
 
 dic = {}
-for i in range(8):
+for i in range(9):
   dic[i + 1] = []
 
+strList = []
 for _ in range(N):
   str = sys.stdin.readline().rstrip()
-  arr = dic.get(len(str))
+  print(str)
+  for i in range(len(str), 0, -1):
+    dic[i].append(str[i - 1])
+
+print(dic)
