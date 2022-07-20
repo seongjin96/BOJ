333379cebe20e85124c99852c4c0f5e5733e00f5
고 성진 <w8348@naver.com>
2022-07-21 08:58:33 +0900

    단어 수학

--- 그리디 알고리즘/1339.py
+++ 그리디 알고리즘/1339.py
@@ -1,5 +1,7 @@
 '''
 단어 수학
+
+자리 수에 맞는 값을 부여한 후 정렬
 '''
 
 import sys
@@ -7,16 +9,29 @@ import sys
 N = int(sys.stdin.readline())
 
 dic = {}
-for i in range(9):
-  dic[i + 1] = []
+answer = 0
+max = 9
 
+# 문자열을 담을 배열
 strList = []
+
 for _ in range(N):
-  str = sys.stdin.readline().rstrip()
-  strLen = len(str)
-  for i in range(len(str)):
-    dic[strLen].append(str[i])
-    strLen -= 1
+  strList.append(sys.stdin.readline().rstrip())
 
+for str in strList:
+  # 제곱값(맨 뒤는 1의 자리이기 때문에 0제곱을 해야하므로 -1을 해준다)
+  root = len(str) - 1
+  for i in str:
+    if i in dic:
+      dic[i] += pow(10, root)
+    else:
+      dic[i] = pow(10, root)
+    root -= 1
 
-print(dic)
+dic = sorted(dic.values(), reverse = True)
+
+for num in dic:
+  answer += num * max
+  max -= 1
+
+print(answer)
